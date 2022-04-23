96efbd522526a529f85821502758172e7dbc1ab0
arc3us <arinjayjain380@gmail.com>
2022-04-23 16:35:00 +0530

    Header init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from "./Header.js"
 
 function App() {
   return (
     <div className="App">
-     <h1>Hello world</h1>
+     <Header />
+     <h2>hello world</h2>
     </div>
   );
 }
