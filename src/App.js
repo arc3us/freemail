1ba02e1ea27c407b3d1e7b4392176385b08cde99
arc3us <arinjayjain380@gmail.com>
2022-04-24 01:48:31 +0530

    Email List and sections init

--- src/App.js
+++ src/App.js
@@ -2,12 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from "./Header.js"
 import Sidebar from './Sidebar';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
+import Mail from "./Mail"
+import EmailList from './EmailList';
 
 function App() {
   return (
     <div className="App">
      <Header /> 
-     <Sidebar />
+     <Router>
+     <div className="app_body">
+      <Sidebar />
+      <Routes>
+         <Route path = "/mail" element={<Mail />} />
+         <Route path="/" element={  <EmailList />} />
+      </Routes>
+     </div>
+     </Router>
     </div>
   );
 }
