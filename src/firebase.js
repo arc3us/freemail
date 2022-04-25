e07af6e8f1b252f07c7d00009d057cb6cd33fbbe
arc3us <arinjayjain380@gmail.com>
2022-04-25 20:17:59 +0530

    Firebase.js redacted

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAkaDUwBj34JpSxoJfJPznZgjK4nMRnYI4",
-  authDomain: "freemail-1337.firebaseapp.com",
-  projectId: "freemail-1337",
-  storageBucket: "freemail-1337.appspot.com",
-  messagingSenderId: "785344344701",
-  appId: "1:785344344701:web:c69557e69b33ec6b9ab714",
-  measurementId: "G-0X7BPMB2D8",
+  
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
